02a2686d5a9efa53a5c2271ddd2c44f415d6cef8
Connor McCarroll <connor@mccarroll.dev>
2023-04-28 22:09:12 +0100

    Initial commit

---
+++ src/generation_settings.rs
@@ -0,0 +1,126 @@
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+pub struct GenerationSettings {
+    /// When enabled, all input formatting options default to false instead of the value in the KoboldAI GUI
+    pub disable_input_formatting: Option<bool>,
+
+    /// When enabled, all output formatting options default to false instead of the value in the KoboldAI GUI.
+    pub disable_output_formatting: Option<bool>,
+
+    /// Input formatting option. When enabled, adds a leading space to your input if there is no trailing whitespace at the end of the previous action. If disable_input_formatting is true, this defaults to false instead of the value in the KoboldAI GUI.
+    #[serde(rename = "frmtadsnsp")]
+    pub add_leading_space: Option<bool>,
+
+    /// Output formatting option. When enabled, replaces all occurrences of two or more consecutive newlines in the output with one newline. If disable_output_formatting is true, this defaults to false instead of the value in the KoboldAI GUI.
+    #[serde(rename = "frmtrmblln")]
+    pub trim_blank_lines: Option<bool>,
+
+    /// Output formatting option. When enabled, removes #/@%{}+=~|\^<> from the output. If disable_output_formatting is true, this defaults to false instead of the value in the KoboldAI GUI.
+    #[serde(rename = "frmtrmspch")]
+    pub trim_special_characters: Option<bool>,
+
+    /// Output formatting option. When enabled, removes some characters from the end of the output such that the output doesn't end in the middle of a sentence. If the output is less than one sentence long, does nothing. If disable_output_formatting is true, this defaults to false instead of the value in the KoboldAI GUI.
+    #[serde(rename = "frmttriminc")]
+    pub trim_incomplete_sentences: Option<bool>,
+
+    /// Maximum number of tokens to send to the model.
+    pub max_context_length: i32,
+
+    /// Number of tokens to generate.
+    pub max_length: i32,
+
+    /// Number of outputs to generate.
+    pub n: i32,
+
+    /// When enabled, Generated output will not be displayed in the console.
+    pub quiet: Option<bool>,
+
+    /// Base repetition penalty value.
+    pub rep_pen: Option<f32>,
+
+    /// Repetition penalty range.
+    pub rep_pen_range: Option<i32>,
+
+    /// Repetition penalty slope.
+    pub rep_pen_slope: Option<f32>,
+
+    /// If enabled, the generated text will always be the same as long as you use the same RNG seed, input and settings. If disabled, only the sequence of generated texts that you get when repeatedly generating text will be the same given the same RNG seed, input and settings.
+    pub sampler_full_determinism: Option<bool>,
+
+    /// Sampler order to be used. If N is the length of this array, then N must be greater than or equal to 6 and the array must be a permutation of the first N non-negative integers.
+    pub sampler_order: Option<Vec<i32>>,
+
+    /// RNG seed to use for sampling. If not specified, the global RNG will be used.
+    pub sampler_seed: Option<i32>,
+
+    /// Output formatting option. When enabled, removes everything after the first line of the output, including the newline. If disable_output_formatting is true, this defaults to false instead of the value in the KoboldAI GUI.
+    #[serde(rename = "singleline")]
+    pub single_line: Option<bool>,
+
+    /// Soft prompt to use when generating. If set to the empty string or any other string containing no non-whitespace characters, uses no soft prompt.
+    pub soft_prompt: Option<String>,
+
+    /// Temperature value.
+    pub temperature: Option<f32>,
+
+    /// Tail free sampling value.
+    pub tfs: Option<f32>,
+
+    /// Top-a sampling value.
+    pub top_a: Option<f32>,
+
+    /// Top-k sampling value.
+    pub top_k: Option<i32>,
+
+    /// Top-p sampling value.
+    pub top_p: Option<f32>,
+
+    /// Typical sampling value.
+    pub typical: Option<f32>,
+
+    // TODO
+    pub use_memory: bool,
+    pub use_story: bool,
+    pub use_authors_note: bool, 
+    pub use_world_info: bool,
+    pub use_userscripts: bool,
+}
+
+impl Default for GenerationSettings {
+    fn default() -> Self {
+        GenerationSettings {
+            disable_input_formatting: Some(true),
+            disable_output_formatting: Some(true),
+            add_leading_space: Some(false),
+            trim_blank_lines: Some(false),
+            trim_special_characters: Some(false),
+            trim_incomplete_sentences: Some(false),
+            max_context_length: 1024,
+            max_length: 80,
+            n: 1,
+            quiet: Some(true),
+            rep_pen: Some(1.1),
+            rep_pen_range: Some(1024),
+            rep_pen_slope: Some(0.7),
+            sampler_full_determinism: Some(false),
+            sampler_order: Some(vec![6, 0, 1, 2, 3, 4, 5]),
+            sampler_seed: Some(0),
+            single_line: Some(false),
+            soft_prompt: Some(String::new()),
+            temperature: Some(0.5),
+            tfs: Some(1.0),
+            top_a: Some(0.0),
+            top_k: Some(0),
+            top_p: Some(0.9),
+            typical: Some(1.0),
+
+            // TODO
+            use_memory: true,
+            use_story: true,
+            use_authors_note: true,
+            use_world_info: true,
+            use_userscripts: true,
+        }
+    }
+}
---
+++ src/model.rs
@@ -0,0 +1,47 @@
+use std::fmt;
+
+#[derive(Debug)]
+pub enum Model {
+    HuggingFace(String, String),
+    Local(String),
+}
+
+impl fmt::Display for Model {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Model::HuggingFace(owner, model) => {
+                write!(f, "{owner}/{model}")
+            },
+            Model::Local(path) => {
+                write!(f, "{path}")
+            },
+        }
+    }
+}
+
+impl From<&str> for Model {
+    fn from(value: &str) -> Self {
+        // TODO: Improve this
+        if value.starts_with("./") {
+            let path = value.split_at(2);
+            Model::Local(String::from(path.1))
+        } else {
+            let values: Vec<&str> = value.split('/').collect();
+            Model::HuggingFace(String::from(values[0]), String::from(values[1]))
+        }
+    }
+}
+impl From<String> for Model {
+    fn from(value: String) -> Self {
+        Model::from(value.as_str())
+    }
+}
+
+impl serde::Serialize for Model {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(&self.to_string())
+    }
+}
